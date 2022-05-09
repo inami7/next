7c2037c9511600cd9e25111e6af335585c6c5076
inami7 <103008905+inami7@users.noreply.github.com>
2022-05-09 16:36:00 +0900

    0509

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import { PasswordDialog } from "./components/page/password-dialog";
 export default function App() {
   return (
     <div className="App">
-      <PasswordDialog />
+      <PasswordDialog
+        onSubmit={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        onClose={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        onCancel={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
     </div>
   );
 }
